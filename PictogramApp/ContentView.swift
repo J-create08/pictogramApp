4bdf566f33a4f1ed89de1ed2a66e40a4538f83d1
Juan Carlos Rojas <rojb.jc@gmail.com>
2021-03-25 21:31:01 -0600

    Initial Commit

---
+++ PictogramApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PictogramApp
+//
+//  Created by Juan Carlos  Rojas on 25/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PictogramApp/PictogramAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictogramAppApp.swift
+//  PictogramApp
+//
+//  Created by Juan Carlos  Rojas on 25/3/21.
+//
+
+import SwiftUI
+
+@main
+struct PictogramAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
